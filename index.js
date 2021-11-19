ac569fdf58a364f555c83519a715257124888334
Nabeil <nabeillemar@gmail.com>
2021-11-18 21:03:26 -0800

    commit tree 9427167a2b033b05404c83f86d86f589008cfda7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function wrapCards(nameArray, string) {
+    let cards = []
+    for (let i = 0; i < nameArray.length; i++) {
+      cards.push(`Wrapped ${nameArray[i]} and added ${string} a bow!`);
+    }
+  
+  
+    return cards;
+  }
